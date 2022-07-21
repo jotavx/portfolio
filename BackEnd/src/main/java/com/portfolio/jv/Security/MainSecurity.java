ec00368c5eb66dff84a98ab43992073fa2c7c7f4
jotavx <jotaviarruel97@gmail.com>
2022-07-21 20:07:16 -0300

    jwt
    Correccion provider y main security

--- BackEnd/src/main/java/com/portfolio/jv/Security/MainSecurity.java
+++ BackEnd/src/main/java/com/portfolio/jv/Security/MainSecurity.java
@@ -7,10 +7,6 @@ import com.portfolio.jv.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.http.HttpMethod;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -62,7 +58,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
